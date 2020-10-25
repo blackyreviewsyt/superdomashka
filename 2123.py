47004ad54e4e595b3582b2aaa1fd93e70a4b39f8
arli <blackreviewsyt@gmail.com>
2020-10-25 16:47:48 +0300

    Initial commit.

---
+++ 2123.py
@@ -0,0 +1,55 @@
+a=int(input("введите число"))
+
+if a < 10 and a > -10:
+    print(a,'+',5,'=',a+5)
+else:
+    print(a,'-',10,'=',a-10)
+
+b=int(input("введите номер месяца"))
+
+if b == 1:
+    print("Январь")
+
+if b == 2:
+    print("февраль")
+
+if b == 3:
+    print("март")
+
+if b == 4:
+    print("апрель")
+
+if b == 5:
+    print("май")
+
+if b == 6:
+    print("июнь")
+
+if b == 7:
+    print("июль")
+
+if b == 8:
+    print("август")
+
+if b == 9:
+    print("сентябрь")
+
+if b == 10:
+    print("октябрь")
+
+if b == 11:
+    print("ноябрь")
+
+if b == 12:
+    print("декабрь")
+
+if b < 1 or b > 12:
+    print("нет такого месяца")
+
+    
+
+
+
+
+
+
